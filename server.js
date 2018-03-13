f8523f66ace58d266e5b6cca7281b90de208ea54
Rafael Wayar <rafael.wt@gmail.com>
2018-03-13 15:09:31 -0400

    primer version funcional

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+//var io = require('socket.io').listen(server);  //linux
+var io = require('socket.io')(server, { wsEngine: 'ws' }).listen(server); //windows
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 8080 );
+console.log('Server running ..');
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',function(socket){
+    connections.push(socket);
+    console.log('Connected: %s socktes connected',connections.length);
+     // disconnect
+    socket.on('disconnect',function(data){
+       
+    connections.splice(connections.indexOf(socket),1);
+    console.log('Disconnected: %s sockets connected',connections.length);
+    });
+
+    //send message
+    socket.on('send message',function(data){
+        io.emit('send message',{msg: data});
+    });
+    
+});
